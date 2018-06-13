d3a80f718245267e38e5ca1330a5a7eb3dedc254
Masami Yonehara <zeitdiebe@gmail.com>
2018-06-13 23:13:48 +0900

    Style correctly when reloading
    We need to apply our styles to html which will be created and provided
    by a server

--- components/layout.js
+++ components/layout.js
@@ -4,9 +4,14 @@ import Head from 'next/head'
 export default ({ children }) => (
   <div>
     <Head>
-      <title></title>
+      <title>test</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <style jsx global>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
     </Head>
 
     { children }
