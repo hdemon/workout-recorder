a1977c79603cf1f75031a7c321b6eb6f3af32c47
Masami Yonehara <zeitdiebe@gmail.com>
2018-06-13 23:13:19 +0900

    Use styled-components

--- components/exercise.js
+++ components/exercise.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 5em;
+    width: 100%;
+    background-color: #111;
+`
+
+const Name = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 32px;
+    color: #eee;
+`
 
 const Exercise = ({ name, weight, reps }) =>
-    <div>
-        <div>{name}</div>
+    <Wrapper>
+        <Name>{name}</Name>
         <div>{weight}</div>
         <div>{reps}</div>
-    </div>
+    </Wrapper>
 
 export default Exercise
